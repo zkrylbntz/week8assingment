ad26247174befd61e6e7673ec4470a9594bb6081
Zak Le Bentz <zakerylebentz@gmail.com>
2024-10-20 14:17:20 +0100

    Add ascending and descending to reviews, sorted out Vercel error

--- src/app/reviews/page.js
+++ src/app/reviews/page.js
@@ -6,13 +6,19 @@ import { db } from "@/utils/dbConnection";
 // We need query string to sort the data asc and desc
 //! Remember the metadata
 
-export default async function ReviewsPage() {
+export default async function ReviewsPage({ searchParams }) {
   const reviews = await db.query(`SELECT * FROM reviews`);
   const wrangledReviews = reviews.rows;
   // Here I need to get the posts from the db
+  if (searchParams.sort === "desc") {
+    wrangledReviews.reverse();
+  }
   return (
     <>
       <h1>Reviews Page</h1>
+      <Link href="/reviews?sort=asc">Sort ascending</Link>
+      <Link href="/reviews?sort=desc">Sort descending</Link>
+
       {/* Here I need a list of my posts */}
       {wrangledReviews.map((review) => (
         <div key={review.id}>
